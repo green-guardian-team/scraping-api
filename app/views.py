ad2b8ee29548954336e335b2c31abf401df87678
henrique221 <hborgesdasilva9294@gmail.com>
2020-05-30 18:05:57 -0300

    Beautifulsoup

--- app/views.py
+++ app/views.py
@@ -1,7 +1,19 @@
 from app import app
-from flask import redirect
+import requests
+from bs4 import BeautifulSoup
+import json
+from flask import Response
 
 
 @app.route('/')
 def home():
-    return redirect('http://google.com')
+    page = requests.get('https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm')
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    artist_name_list = soup.find(class_='BodyText')
+    artist_name_list_items = artist_name_list.find_all('a')
+    list = []
+    for artist_name in artist_name_list_items:
+        list.append(artist_name.prettify())
+        
+    return json.dumps(list)
