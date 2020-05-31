89f76dd51f6692cc05823d9841a2094acdac2385
rodrigoctoledo <rodrigo_toledo@outlook.com>
2020-05-31 17:55:59 -0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+  
 from bs4 import BeautifulSoup
 import json
 import logging
@@ -6,6 +7,7 @@ import requests
 from app import app
 from utils import response, dateFormatter
 from services import newsFromWHO
+from services import newsFromblog
 
 @app.route('/api/news/', methods=['GET'])
 def news():
@@ -15,6 +17,19 @@ def news():
         resultWHO = newsFromWHO.searchNews(parameter)
 
         return response.response(resultWHO)
+        
+
+    except Exception as e:
+        return str(e)
+        
+
+def newsnew():
+    try:
+        parameterblog =  request.args.get('parameter', 'covid')
+
+        resultblog = newsFromblog.searchNews(parameterblog)
+
+        return response.response(resultblog)
         
 
     except Exception as e:
