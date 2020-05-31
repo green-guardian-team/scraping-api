2fca57d41c73079f90c72ec50e82a174f6ed15ff
henrique221 <hborgesdasilva9294@gmail.com>
2020-05-31 20:47:58 -0300

    revert

--- app/views.py
+++ app/views.py
@@ -5,7 +5,11 @@ from flask import Response, request, Flask, jsonify
 import requests
 from app import app
 from utils import response, dateFormatter
+<<<<<<< HEAD
 from services import newsFromWHO, newsFromGlobo, NewFromGOV
+=======
+from services import newsFromWHO
+>>>>>>> parent of 31d9b66... Merge pull request #4 from green-guardian-team/develop/news
 
 @app.route('/api/news/', methods=['GET'])
 def news():
@@ -13,16 +17,17 @@ def news():
         parameter =  request.args.get('parameter', 'covid')
         
         resultWHO = newsFromWHO.searchNews(parameter)
+<<<<<<< HEAD
         resultGlobo = newsFromGlobo.searchNews(parameter)
         resultGOV = NewFromGOV.searchNews(parameter)
+=======
+>>>>>>> parent of 31d9b66... Merge pull request #4 from green-guardian-team/develop/news
 
         list = {"news": []}
 
         for i in range(0, len(resultWHO)):
             list["news"].append(resultWHO[i])
 
-        for i in range(0, len(resultGlobo)):
-            list["news"].append(resultGlobo[i])
 
         for i in range(0, len(resultGOV)):
             list["news"].append(resultGOV[i])
